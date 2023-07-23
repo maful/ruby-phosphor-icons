3006907a9bf50f1e640bbf5651d307a35e223fed
maful <me@maful.web.id>
2023-07-23 10:43:06 +0700

    feat: add options to set height and width

--- lib/phosphor_icons/icon.rb
+++ lib/phosphor_icons/icon.rb
@@ -4,19 +4,25 @@ module PhosphorIcons
   class Icon
     DEFAULT_STYLE = "regular"
     SUPPORTED_STYLES = [DEFAULT_STYLE, "bold", "light", "duotone", "fill", "thin"]
+    NATURAL_HEIGHTS = [16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80]
+    DEFAULT_HEIGHT = 24
 
-    attr_reader :symbol, :style, :options, :path
+    attr_reader :symbol, :style, :options, :path, :width, :height
 
     def initialize(symbol, options = {})
       @symbol = symbol.to_s
       @style = options[:style] || DEFAULT_STYLE
       if (phosphor_icon = get_phosphor_icon(@symbol, options))
         @path = phosphor_icon["path"]
+        @width = phosphor_icon["width"]
+        @height = phosphor_icon["height"]
         @options = {
           class: "phosphor-icon #{options[:class]}".strip,
           viewBox: viewbox,
           xmlns: "http://www.w3.org/2000/svg",
           fill: "currentColor",
+          width: @width,
+          height: @height,
         }
       else
         raise IconNotFoundError, "Couldn't find #{self}"
@@ -45,10 +51,14 @@ module PhosphorIcons
 
     def get_phosphor_icon(symbol, options = {})
       if (phosphor_icon = PhosphorIcons::ICON_SYMBOLS[symbol])
+        height = options[:height] || options[:width] || DEFAULT_HEIGHT
+        natural_height = closest_natural_height(height)
         style = get_style(options[:style])
 
         {
           "name" => phosphor_icon["name"],
+          "width" => natural_height,
+          "height" => natural_height,
           "path" => phosphor_icon["styles"][style.to_s]["path"],
         }
       end
@@ -58,6 +68,12 @@ module PhosphorIcons
 
     def get_style(style)
       SUPPORTED_STYLES.include?(style.to_s) ? style : DEFAULT_STYLE
+    end
+
+    def closest_natural_height(height)
+      NATURAL_HEIGHTS.reduce(NATURAL_HEIGHTS[0].to_i) do |acc, natural_height|
+        natural_height.to_i <= height.to_i ? natural_height.to_i : acc
+      end
     end
   end
 end
--- test/test_phosphor_icons.rb
+++ test/test_phosphor_icons.rb
@@ -45,4 +45,28 @@ class TestPhosphorIcons < Minitest::Test
     icon = phosphor_icon(:alarm, class: "foo")
     assert_includes(icon.to_svg, "class=\"phosphor-icon foo\"")
   end
+
+  def test_default_height
+    icon = phosphor_icon(:alarm)
+    assert_equal(24, icon.height)
+    assert_equal(24, icon.width)
+    assert_includes(icon.to_svg, "height=\"24\"")
+    assert_includes(icon.to_svg, "width=\"24\"")
+  end
+
+  def test_custom_height
+    icon = phosphor_icon(:alarm, height: 16)
+    assert_equal(16, icon.height)
+    assert_equal(16, icon.width)
+    assert_includes(icon.to_svg, "height=\"16\"")
+    assert_includes(icon.to_svg, "width=\"16\"")
+  end
+
+  def test_custom_height_with_closest_natural_height
+    icon = phosphor_icon(:alarm, height: 21)
+    assert_equal(20, icon.height)
+    assert_equal(20, icon.width)
+    assert_includes(icon.to_svg, "height=\"20\"")
+    assert_includes(icon.to_svg, "width=\"20\"")
+  end
 end
